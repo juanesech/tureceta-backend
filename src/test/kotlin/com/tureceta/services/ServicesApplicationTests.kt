8718ad63ec0b54c17454fe4c79749dfd3c1f1e9a
Juan Esteban Echavarria <juan.echavarria@techandsolve.com>
2018-07-09 10:10:08 -0500

    Spring boot

---
+++ src/test/kotlin/com/tureceta/services/ServicesApplicationTests.kt
@@ -0,0 +1,16 @@
+package com.tureceta.services
+
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.junit4.SpringRunner
+
+@RunWith(SpringRunner::class)
+@SpringBootTest
+class ServicesApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
